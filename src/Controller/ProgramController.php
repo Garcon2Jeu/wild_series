22a51b2f50d60b35472137bad9a339353ba1c1e0
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-05-23 15:09:37 +0200

    finished quest 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,23 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
+use App\Entity\Program;
 
 #[Route("/program/", name: "program_")]
 class ProgramController extends AbstractController
 {
-    #[Route("/", methods: ["GET"], name: "index")]
-    public function index(): Response
+    #[Route("", methods: ["GET"], name: "index")]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render("program/index.html.twig", [
-            "website" => "Wild Series"
+            "programs" => $programs
         ]);
     }
 
     #[Route("{id<\d+>}", methods: ["GET"], name: "show")]
-    public function show(int $id)
+    public function show(int $id, ProgramRepository $programRepository)
     {
+        $program = $programRepository->findOneBy(["id" => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                "no program with id : " . $id . " found in program's table."
+            );
+        }
+
         return $this->render("program/showID.html.twig", [
-            "id" => $id
+            "program" => $program
         ]);
     }
 }
