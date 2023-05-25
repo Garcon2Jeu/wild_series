2976c3cc4d2f0a660903595fb1754064bd9e3355
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-05-25 17:30:23 +0200

    Finished quest 14

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,14 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/category/", methods: ["GET"], name: "category_")]
+#[Route("/category/", name: "category_")]
 class CategoryController extends AbstractController
 {
+    #[Route("new", methods: ["GET", "POST"], name: "new")]
+    public function new(
+        Request $request,
+        CategoryRepository $categoryRepository
+    ): Response {
+
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $category = Category::withName($request->request->all()["category"]["name"]);
+            $categoryRepository->save($category, true);
+            return $this->redirectToRoute("category_index");
+        }
+
+        return $this->render("category/new.html.twig", [
+            "form" => $form
+        ]);
+    }
+
     #[Route("", name: 'index')]
     public function index(CategoryRepository $categoryRepository): Response
     {
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Episode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +10,36 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route("/program/", name: "program_")]
 class ProgramController extends AbstractController
 {
+    #[Route("new", methods: ["GET", "POST"], name: "new")]
+    public function new(
+        Program $program,
+        Request $request,
+        CategoryRepository $categoryRepository,
+        ProgramRepository $programRepository
+    ) {
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $postData = $request->request->all()["program"];
+            $postData["category"] = $categoryRepository->find(["id" => $postData["category"]]);
+
+            $program = Program::withData($postData);
+            $programRepository->save($program, true);
+
+            return $this->redirectToRoute("program_index");
+        }
+
+        return $this->render("program/new.html.twig", ["form" => $form]);
+    }
+
     #[Route("", methods: ["GET"], name: "index")]
     public function index(ProgramRepository $programRepository): Response
     {
